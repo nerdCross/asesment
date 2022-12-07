3a65c2bfacec9113079b38bd5ccea189202ff133
nerdCross <processordamilola@gmail.com>
2022-12-07 12:19:00 +0000

    v3

--- generator.py
+++ generator.py
@@ -1,16 +1,23 @@
 from pipelines import pipeline
 import requests
+import os
+cmd = "python -m nltk.downloader punkt"
+returned_value = os.system(cmd)
 nlp = pipeline("question-generation", model="valhalla/t5-base-qg-hl")
-
 def gen(resource_link):
-    url = 'https://ec2-18-134-208-139.eu-west-2.compute.amazonaws.com/transpile'
-    data = resource_link
-    transcribed_text = requests.post(url, data=data)
-    print (transcribed_text)
-    questions_and_ans = nlp(transcribed_text)
-    print (questions_and_ans)
-    return questions_and_ans
+    url = 'http://3.8.100.147/transpile/'
+    myobj = {"resource_link": "https://relen.s3.us-east-2.amazonaws.com/audio/samples/sample3.wav"}
+    myobj["resource_link"] = resource_link
 
-resource_link = "https://relen.s3.us-east-2.amazonaws.com/audio/samples/sample3.wav"
-gen(resource_link)
+    print("Dictionary = ",myobj)
 
+    #x = requests.post(url, json = myobj)
+    # Inserting a key that already exists, updates only the values
+
+    #print the response text (the content of the requested file):
+
+    #questions_and_ans = nlp(transcribed_text)
+    #print (questions_and_ans)
+    #return questions_and_ans
+link = "https://relen.s3.us-east-2.amazonaws.com/audio/samples/sample5.wav"
+gen(link)
