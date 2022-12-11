3e6115d3192e20622ba325e66db1a13c76236676
nerdCross <processordamilola@gmail.com>
2022-12-11 05:36:25 +0000

    v5

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
-        title="RELEN Smart API documentation",
+        title="RELEN Smart Assessment API documentation",
         version="0.0.1",
-        description="This api and Documentation was developed by processor",
+        description="This API and Documentation was developed by processor",
         routes=app.routes,
     )
     openapi_schema["info"]["x-logo"] = {
-        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+        "url": "/static/logo-teal.png"
     }
     app.openapi_schema = openapi_schema
     return app.openapi_schema
